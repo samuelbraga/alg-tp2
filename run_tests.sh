4176f130828bc8ae264f83a1a9a05aef9f784315
Samuel Braga <samuel.fantini.braga@hotmail.com>
2021-02-19 20:34:01 -0300

    Conclusao dos testes

--- run_tests.sh
+++ run_tests.sh
@@ -2,7 +2,7 @@ EXEC=$1
 TMP_OUT=$2
 
 echo "Executando testes:"
-for i in {1..1..1}; do
+for i in {1..4..1}; do
   testname=$(printf "%02d" $i)
   $EXEC < tests/$testname.in > $TMP_OUT
   if ! diff -qwB tests/$testname.out $TMP_OUT &>/dev/null; then
